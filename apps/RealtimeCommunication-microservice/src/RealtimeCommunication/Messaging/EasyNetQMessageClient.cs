e6a9d003f657f8a9b032ab86df42251d8a6d71b8
Lukas <lukas@askholm.net>
2026-09-10 11:56:07 +0200

    add easynetq

---
+++ apps/RealtimeCommunication-microservice/src/RealtimeCommunication/Messaging/EasyNetQMessageClient.cs
@@ -0,0 +1,45 @@
+﻿using EasyNetQ;
+
+namespace RealtimeCommunication.Messaging;
+
+public sealed class EasyNetQMessageClient : IMessageClient, IAsyncDisposable
+{
+    private readonly IBus _bus;
+    private readonly List<IAsyncDisposable> _subscriptions = [];
+
+    public EasyNetQMessageClient(IBus bus)
+    {
+        _bus = bus;
+    }
+
+    public Task PublishAsync<TMessage>(
+        TMessage message,
+        CancellationToken cancellationToken = default)
+    {
+        return _bus.PubSub.PublishAsync(
+            message,
+            cancellationToken);
+    }
+
+    public async Task SubscribeAsync<TMessage>(
+        string subscriptionId,
+        Func<TMessage, CancellationToken, Task> handler,
+        CancellationToken cancellationToken = default)
+    {
+        var subscription = await _bus.PubSub.SubscribeAsync(
+            subscriptionId,
+            handler,
+            _ => { },
+            cancellationToken);
+
+        _subscriptions.Add(subscription);
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        foreach (var subscription in _subscriptions)
+        {
+            await subscription.DisposeAsync();
+        }
+    }
+}
---
+++ apps/RealtimeCommunication-microservice/src/RealtimeCommunication/Messaging/IMessageClient.cs
@@ -0,0 +1,13 @@
+﻿namespace RealtimeCommunication.Messaging;
+
+public interface IMessageClient
+{
+    Task PublishAsync<TMessage>(
+        TMessage message,
+        CancellationToken cancellationToken = default);
+
+    Task SubscribeAsync<TMessage>(
+        string subscriptionId,
+        Func<TMessage, CancellationToken, Task> handler,
+        CancellationToken cancellationToken = default);
+}
